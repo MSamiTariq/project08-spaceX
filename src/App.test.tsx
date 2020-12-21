9dd561cecba7a80e9ca202ad80840fe3bd42793c
samitariq12 <samitariq40@gmail.com>
2020-12-21 19:00:04 +0500

    checking test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,10 +5,17 @@ import Header from "./components/Header";
 
 describe("App Component renders", () => {
 	let container = shallow(<App />);
+	console.log(container.debug());
 	it("should render a div", () => {
 		expect(container.find("div").length).toEqual(1);
 	});
 	it("should render the Header component", () => {
 		expect(container.containsMatchingElement(<Header />)).toEqual(true);
+	});
+	it("should contain routes tag", () => {
+		expect(container.find("Routes").length).toEqual(1);
+	});
+	it("should contain routes tag", () => {
+		expect(container.find("Routed").length).toEqual(3);
 	});
 });
